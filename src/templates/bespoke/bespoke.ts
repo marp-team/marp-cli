ee6e91b9744935a1306109b855455687c2ee1135
Yuki Hattori <yukihattori1116@gmail.com>
2018-08-26 06:01:22 +0900

    Add bespoke cursor plugin to hide inactive cursor

--- src/templates/bespoke/bespoke.ts
+++ src/templates/bespoke/bespoke.ts
@@ -1,11 +1,13 @@
 import bespoke from 'bespoke'
 import bespokeClasses from './classes'
+import bespokeCursor from './cursor'
 import bespokeHash from './hash'
 import bespokeNavigation from './navigation'
 
 export default function() {
   bespoke.from(document.getElementById('presentation'), [
     bespokeClasses,
+    bespokeCursor(),
     bespokeHash({ history: false }),
     bespokeNavigation,
   ])
