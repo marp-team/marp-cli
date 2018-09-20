1e045bd8df1914b8d998941f2a7c20b033a10600
Yuki Hattori <yukihattori1116@gmail.com>
2018-09-20 18:17:47 +0900

    Migrate jest configuration file for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const { jestPreset } = require('ts-jest')
+
 module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '.*\\.d\\.ts'],
@@ -5,7 +7,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   setupFiles: ['jest-plugin-context/setup'],
   transform: {
-    '^.*\\.ts$': 'ts-jest',
+    ...jestPreset.transform,
     '^.*\\.s?css$': '<rootDir>/test/_transformers/css.js',
     '^.*\\.pug$': '<rootDir>/test/_transformers/pug.js',
   },
