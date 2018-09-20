e836975064d114538b49e6557e5348d04ce9e08a
Yuki Hattori <yukihattori1116@gmail.com>
2018-09-20 18:33:37 +0900

    Use Object.assign instead of spread syntax in jest.config.js
    Node 6 is not supported the object spread syntax natively.

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,10 @@ module.exports = {
   coverageThreshold: { global: { lines: 95 } },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   setupFiles: ['jest-plugin-context/setup'],
-  transform: {
-    ...jestPreset.transform,
+  transform: Object.assign({}, jestPreset.transform, {
     '^.*\\.s?css$': '<rootDir>/test/_transformers/css.js',
     '^.*\\.pug$': '<rootDir>/test/_transformers/pug.js',
-  },
+  }),
   testEnvironment: 'node',
   testRegex: '(/(test|__tests__)/(?!_).*|(\\.|/)(test|spec))\\.[jt]s$',
 }
