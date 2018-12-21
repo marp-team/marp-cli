f8462392110599a7edf25345f28708170db4d324
Yuki Hattori <yukihattori1116@gmail.com>
2018-12-21 11:23:23 +0900

    Fix to toggle fullscreen twice by hitting F11 in Firefox

--- src/templates/bespoke/fullscreen.ts
+++ src/templates/bespoke/fullscreen.ts
@@ -3,7 +3,9 @@ import screenfull from 'screenfull'
 export default function bespokeFullscreen({ parent }) {
   document.addEventListener('keydown', e => {
     // `f` or F11
-    if ((e.which === 70 || e.which === 122) && screenfull.enabled)
+    if ((e.which === 70 || e.which === 122) && screenfull.enabled) {
       screenfull.toggle(parent)
+      e.preventDefault()
+    }
   })
 }
