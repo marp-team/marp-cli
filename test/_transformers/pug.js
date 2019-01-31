ffbc66b4b001b6fed467ded6bac182e2ceb1d550
Yuki Hattori <yukihattori1116@gmail.com>
2019-01-31 19:13:08 +0900

    Update pug transformer to pass filename

--- test/_transformers/pug.js
+++ test/_transformers/pug.js
@@ -1,10 +1,10 @@
 const pug = require('pug')
 
 module.exports = {
-  process: src => `
+  process: (src, filename) => `
 module.exports = (locals) => {
   const pug = require('pug').runtime;
-  ${pug.compile(src).toString()}
+  ${pug.compile(src, { filename }).toString()}
   return template(locals);
 };`,
 }
