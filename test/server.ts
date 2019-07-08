b9e7b1425fadb9fb62e3b15d03e739a04756a5b3
Yuki Hattori <yukihattori1116@gmail.com>
2019-07-08 17:53:23 +0900

    Update test of serving PPTX

--- test/server.ts
+++ test/server.ts
@@ -77,16 +77,16 @@ describe('Server', () => {
   })
 
   describe('Routing', () => {
-    const startServer = async (opts: Server.Options = {}) => {
-      const server = new Server(converter(), opts)
-      await server.start()
+    const setupServer = (opts: Server.Options = {}): Server => {
+      const server: any = new Server(converter(), opts)
 
+      server.setup() // Setup server without listening
       return server
     }
 
     context('when there is request to a served markdown file', () => {
       it('triggers conversion and returns the content of converted HTML', async () => {
-        const server = await startServer()
+        const server = setupServer()
         const cvt = jest.spyOn(server.converter, 'convertFile')
         const response = await request(server.server).get('/1.md')
 
@@ -101,7 +101,7 @@ describe('Server', () => {
         it('emits `converted` event after conversion', async () => {
           let ret: string | undefined
 
-          const server = (await startServer()).on('converted', converted => {
+          const server = setupServer().on('converted', converted => {
             ret = converted.newFile!.buffer!.toString()
           })
 
@@ -112,26 +112,42 @@ describe('Server', () => {
 
       context('with query parameter', () => {
         it('triggers conversion with corresponded type option', async () => {
-          const server = await startServer()
+          const server = setupServer()
 
           jest
             .spyOn<any, any>(server.converter, 'convertFile')
             .mockResolvedValue({ newFile: { buffer: 'converted' } })
 
-          await request(server.server).get('/1.md')
+          const mdRes = await request(server.server).get('/1.md')
           expect(server.converter.options.type).toBe(ConvertType.html)
+          expect(mdRes.type).toBe('text/html')
 
-          await request(server.server).get('/1.md?pdf')
+          const pdfRes = await request(server.server).get('/1.md?pdf')
           expect(server.converter.options.type).toBe(ConvertType.pdf)
+          expect(pdfRes.type).toBe('application/pdf')
 
-          await request(server.server).get('/1.md?png')
+          const pptxRes = await request(server.server).get('/1.md?pptx')
+          expect(server.converter.options.type).toBe(ConvertType.pptx)
+          expect(pptxRes.type).toBe(
+            'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+          )
+
+          // PPTX document is downloaded with a proper filename because it cannot show in browser window.
+          expect(pptxRes.header['content-disposition']).toBe(
+            'attachment; filename="1.pptx"'
+          )
+
+          const pngRes = await request(server.server).get('/1.md?png')
           expect(server.converter.options.type).toBe(ConvertType.png)
+          expect(pngRes.type).toBe('image/png')
 
-          await request(server.server).get('/1.md?jpg')
+          const jpgRes = await request(server.server).get('/1.md?jpg')
           expect(server.converter.options.type).toBe(ConvertType.jpeg)
+          expect(jpgRes.type).toBe('image/jpeg')
 
-          await request(server.server).get('/1.md?jpeg')
+          const jpegRes = await request(server.server).get('/1.md?jpeg')
           expect(server.converter.options.type).toBe(ConvertType.jpeg)
+          expect(jpegRes.type).toBe('image/jpeg')
         })
       })
 
@@ -139,7 +155,7 @@ describe('Server', () => {
         it('returns 503 with error response and emitting event', async () => {
           const err = new Error('test')
           const event = jest.fn()
-          const { server, converter } = (await startServer()).on('error', event)
+          const { server, converter } = setupServer().on('error', event)
 
           jest.spyOn(converter, 'convertFile').mockRejectedValue(err)
 
@@ -153,7 +169,7 @@ describe('Server', () => {
 
     context('when there is request to served directory', () => {
       it('shows the directory index with assigned class by kind', async () => {
-        const server = await startServer()
+        const server = setupServer()
         const response = await request(server.server).get('/')
         expect(response.status).toBe(200)
 
@@ -164,13 +180,14 @@ describe('Server', () => {
         expect($('ul#index li.directory.nodeModules')).toHaveLength(1)
         expect($('ul#index li.convertible')).toHaveLength(3) // Markdown files
 
-        // PDF query parameter
+        // Link to query parameters
         expect($('li a[href*="?pdf"]')).toHaveLength(3)
+        expect($('li a[href*="?pptx"]')).toHaveLength(3)
       })
 
       context('with specified directoryIndex costructor option', () => {
         it('serves the found convertible markdown', async () => {
-          const server = await startServer({ directoryIndex: ['1.md'] })
+          const server = setupServer({ directoryIndex: ['1.md'] })
           const response = await request(server.server).get('/')
 
           expect(response.status).toBe(200)
@@ -183,7 +200,7 @@ describe('Server', () => {
 
     context('when there is request to a static file', () => {
       it('returns the content of static file', async () => {
-        const server = await startServer()
+        const server = setupServer()
         const response = await request(server.server).get('/4.txt')
 
         expect(response.status).toBe(200)
@@ -193,7 +210,7 @@ describe('Server', () => {
 
     context('when the requested file is not found', () => {
       it('returns 404', async () => {
-        const server = await startServer()
+        const server = setupServer()
         const response = await request(server.server).get('/__NOT_FOUND__')
 
         expect(response.status).toBe(404)
@@ -202,7 +219,7 @@ describe('Server', () => {
 
     context('when the directory traversal attack is detected', () => {
       it('returns 403', async () => {
-        const server = await startServer()
+        const server = setupServer()
         const response = await request(server.server).get('/../../README.md')
 
         expect(response.status).toBe(403)
