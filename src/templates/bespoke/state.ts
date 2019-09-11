b3c10c4c91d53e57bb2401dabc81b4554e7e74fd
Yuki Hattori <yuki.hattori@speee.jp>
2019-09-11 13:47:43 +0900

    Keep fragment progress to "f" query param

--- src/templates/bespoke/state.ts
+++ src/templates/bespoke/state.ts
@@ -1,38 +1,92 @@
-// Based on https://github.com/bespokejs/bespoke-hash
+import { generateURLfromParams, readQuery } from './utils'
 
 export interface BespokeStateOption {
   history?: boolean
 }
 
+const coerceInt = (ns: string) => {
+  const coerced = Number.parseInt(ns, 10)
+  return Number.isNaN(coerced) ? null : coerced
+}
+
 export default function bespokeState(opts: BespokeStateOption = {}) {
-  const options: BespokeStateOption = {
-    history: true,
-    ...opts,
-  }
+  const options: BespokeStateOption = { history: true, ...opts }
+
+  const updateState = (...args: Parameters<typeof history['pushState']>) =>
+    options.history ? history.pushState(...args) : history.replaceState(...args)
 
   return deck => {
-    function activateSlide(index) {
-      const idx = Math.max(0, Math.min(index, deck.slides.length - 1))
-      if (idx !== deck.slide()) deck.slide(idx)
+    let internalNavigation = true
+
+    const navInternally = (action: () => any) => {
+      const previous = internalNavigation
+
+      try {
+        internalNavigation = true
+        return action()
+      } finally {
+        internalNavigation = previous
+      }
     }
 
-    function parseHash() {
-      const pageNumber = parseInt(window.location.hash.slice(1), 10)
-      if (!Number.isNaN(pageNumber)) activateSlide(pageNumber - 1)
+    const activateSlide = (index: number, fragment: number | null) => {
+      const { fragments, slides } = deck
+      const idx = Math.max(0, Math.min(index, slides.length - 1))
+      const frag = Math.max(0, Math.min(fragment || 0, fragments.length - 1))
+
+      if (idx !== deck.slide() || frag !== deck.fragmentIndex)
+        deck.slide(idx, { fragment: frag })
     }
+
+    const parseState = (opts: any = { fragment: true }) => {
+      const page = (coerceInt(location.hash.slice(1)) || 1) - 1
+      const fragment = opts.fragment ? coerceInt(readQuery('f') || '') : null
+
+      activateSlide(page, fragment)
+    }
+
+    deck.on('fragment', ({ index, fragmentIndex }) => {
+      if (internalNavigation) return
+
+      const params = new URLSearchParams(location.search)
+
+      if (fragmentIndex === 0) {
+        params.delete('f')
+      } else {
+        params.set('f', fragmentIndex.toString())
+      }
+
+      updateState(
+        null,
+        document.title,
+        generateURLfromParams(params, { ...location, hash: `#${index + 1}` })
+      )
+    })
 
     setTimeout(() => {
-      parseHash()
+      parseState()
 
-      deck.on('activate', e => {
-        if (options.history) {
-          window.location.hash = e.index + 1
-        } else {
-          window.location.replace(`#${e.index + 1}`)
-        }
+      window.addEventListener('hashchange', () =>
+        navInternally(() => {
+          parseState({ fragment: false })
+
+          // f parameter has to remove
+          const params = new URLSearchParams(location.search)
+          params.delete('f')
+
+          history.replaceState(
+            null,
+            document.title,
+            generateURLfromParams(params)
+          )
+        })
+      )
+
+      window.addEventListener('popstate', () => {
+        if (!internalNavigation) navInternally(() => parseState())
       })
 
-      window.addEventListener('hashchange', parseHash)
+      internalNavigation = false
     }, 0)
   }
 }
