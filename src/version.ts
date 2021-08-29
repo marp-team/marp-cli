7f1253c2bc602a545bf4fac6a6a30aca35366dfa
Yuki Hattori <yukihattori1116@gmail.com>
2021-08-29 14:12:56 +0900

    Improve detection of user-installed Marp Core

--- src/version.ts
+++ src/version.ts
@@ -1,15 +1,17 @@
 import { version as bundledCoreVer } from '@marp-team/marp-core/package.json'
 import { name, version } from '../package.json'
 import { MarpCLIConfig } from './config'
+import { ResolvedEngine } from './engine'
 
-export const isMarpCore = async (klass: any): Promise<boolean> =>
-  klass === (await import('@marp-team/marp-core')).Marp
+export const isMarpCore = async (engine: ResolvedEngine): Promise<boolean> =>
+  engine.package?.name === '@marp-team/marp-core' ||
+  engine === (await ResolvedEngine.resolveDefaultEngine())
 
 export default async function outputVersion(config: MarpCLIConfig): Promise<0> {
   let engineVer = ''
   const { engine } = config
 
-  if (await isMarpCore(engine.klass)) {
+  if (await isMarpCore(engine)) {
     engineVer = `@marp-team/marp-core v${bundledCoreVer}`
 
     if (engine.package && engine.package.version !== bundledCoreVer) {
