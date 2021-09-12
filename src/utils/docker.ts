939cd708a0f24f16db9dc8a97a33e5cd31aacd88
Yuki Hattori <yukihattori1116@gmail.com>
2021-09-12 19:54:00 +0900

    Refactor docker util

--- src/utils/docker.ts
+++ src/utils/docker.ts
@@ -1,9 +1,4 @@
 import _isDocker from 'is-docker'
 
-export const isDocker = () => {
-  return isOfficialImage() || _isDocker()
-}
-
-export const isOfficialImage = () => {
-  return !!process.env.MARP_USER
-}
+export const isDocker = () => isOfficialImage() || _isDocker()
+export const isOfficialImage = () => !!process.env.MARP_USER
