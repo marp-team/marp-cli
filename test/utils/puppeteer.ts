41fac5c57dd07e16c064b772e6eea598af4a56e5
Yuki Hattori <yukihattori1116@gmail.com>
2022-01-08 11:35:51 +0900

    Update test for #generatePuppeteerDataDirPath

--- test/utils/puppeteer.ts
+++ test/utils/puppeteer.ts
@@ -31,11 +31,23 @@ beforeEach(() => jest.resetModules())
 afterEach(() => jest.restoreAllMocks())
 
 describe('#generatePuppeteerDataDirPath', () => {
-  it('returns data dir path for OS specific temporary directory', async () => {
+  let mkdirSpy: jest.SpyInstance
+
+  beforeEach(async () => {
+    const { promises } = await import('fs')
+
+    mkdirSpy = jest.spyOn(promises, 'mkdir')
+    mkdirSpy.mockImplementation(() => Promise.resolve(undefined))
+  })
+
+  it('returns the path of created data dir for OS specific temporary directory', async () => {
     const dataDir = await puppeteerUtils().generatePuppeteerDataDirPath(
       'tmp-name'
     )
-    expect(dataDir).toBe(path.resolve(os.tmpdir(), 'tmp-name'))
+    const expectedDir = path.resolve(os.tmpdir(), 'tmp-name')
+
+    expect(dataDir).toBe(expectedDir)
+    expect(mkdirSpy).toHaveBeenCalledWith(expectedDir, { recursive: true })
   })
 
   describe('with wslPath option', () => {
