21d0ea18524c2a3199d3d01649cc1b12300a3d7c
Yuki Hattori <yukihattori1116@gmail.com>
2024-09-27 04:26:49 +0900

    Disable container detection if running inside CI environment

--- src/utils/container.ts
+++ src/utils/container.ts
@@ -1,6 +1,6 @@
 import _isInsideContainer from 'is-inside-container'
 
 export const isInsideContainer = () =>
-  isOfficialDockerImage() || _isInsideContainer()
+  isOfficialDockerImage() || (!process.env.CI && _isInsideContainer())
 
 export const isOfficialDockerImage = () => !!process.env.MARP_USER
