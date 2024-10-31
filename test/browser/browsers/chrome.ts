5ca2a2e2febfbc68cc2eacb044fee4d8d0040ec7
Yuki Hattori <yukihattori1116@gmail.com>
2024-10-31 23:41:49 +0900

    Fix test

--- test/browser/browsers/chrome.ts
+++ test/browser/browsers/chrome.ts
@@ -68,7 +68,9 @@ describe('ChromeBrowser', () => {
 
         expect(puppeteer.launch).toHaveBeenCalledWith(
           expect.objectContaining({
-            args: [expect.stringMatching(/^--user-data-dir=/), '--test-type'],
+            args: expect.arrayContaining([
+              expect.stringMatching(/^--user-data-dir=/),
+            ]),
             ignoreDefaultArgs: [],
           })
         )
