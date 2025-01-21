4690bb4cc9299c8633272d406824caa9ac9136e9
Yuki Hattori <yukihattori1116@gmail.com>
2025-01-21 22:50:13 +0900

    Disable GPU acceleration in CI tests to mitigate flakiness

--- test/browser/browsers/chrome.ts
+++ test/browser/browsers/chrome.ts
@@ -132,6 +132,17 @@ describe('ChromeBrowser', () => {
       })
 
       describe('Disabling GPU', () => {
+        it('does not add --disable-gpu argument if CHROME_DISABLE_GPU environment variable is not defined', async () => {
+          delete process.env.CHROME_DISABLE_GPU
+          await new ChromeBrowser({ path: '/path/to/chrome' }).launch()
+
+          expect(puppeteer.launch).toHaveBeenCalledWith(
+            expect.objectContaining({
+              args: expect.not.arrayContaining(['--disable-gpu']),
+            })
+          )
+        })
+
         it('adds --disable-gpu argument if CHROME_DISABLE_GPU environment variable is defined', async () => {
           process.env.CHROME_DISABLE_GPU = '1'
           await new ChromeBrowser({ path: '/path/to/chrome' }).launch()
