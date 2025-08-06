76fbc943f5b7a97be973090cbdb12296adc403f8
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-06 16:27:50 -0400

    spelling: bespoke
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/templates/bespoke/progress.ts
+++ src/templates/bespoke/progress.ts
@@ -1,7 +1,7 @@
 // Based on https://github.com/bespokejs/bespoke-progress
 const query = '.bespoke-progress-parent > .bespoke-progress-bar'
 
-const basepokeProgress = (deck) => {
+const bespokeProgress = (deck) => {
   deck.on('activate', (e) => {
     document.querySelectorAll<HTMLElement>(query).forEach((bar) => {
       bar.style.flexBasis = `${(e.index * 100) / (deck.slides.length - 1)}%`
@@ -9,4 +9,4 @@ const basepokeProgress = (deck) => {
   })
 }
 
-export default basepokeProgress
+export default bespokeProgress
